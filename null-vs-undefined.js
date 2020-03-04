ad5e8cd12fd91c92da0aa54a5d499c5ae1dfbe34
khrizvi <kashfihasanrizvi@gmail.com>
2020-03-04 21:31:43 +0600

    git added

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+Undefined
+let name
+console.log(name);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+function add(num1, num2){
+    console.log(num1);
+}
+const student = {name: 'kashem', phone: 019879}
+console.log(student.id);
+
+const result = undefined;
+console.log(result);
+
+let num = [23, 12, 45, 23];
+console.log(num[6]);
+
+
+
